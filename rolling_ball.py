7728d400504f2e547badf18491dd5ffc4e1fcea4
Lubeck <elubeck@caltech.edu>
2017-06-25 17:15:22 -0700

    Updated to use JIT

--- rolling_ball.py
+++ rolling_ball.py
@@ -1,5 +1,9 @@
 import math
 import numpy
+from numba import jit
+from parakeet import jit as jitp
+from skimage.morphology import disk
+from skimage.filter import rank
 
 """
 Ported to Python from ImageJ's Background Subtractor.
@@ -18,89 +22,162 @@ reachable by the ball is the background.
 http://rsbweb.nih.gov/ij/developer/source/ij/plugin/filter/BackgroundSubtracter.java.html
 """
 
-
-def smooth(array, window=3.0):
-    """
-    Applies a 3x3 mean filter to specified array.
-    """
-    dx, dy = array.shape
-    new_array = numpy.copy(array)
-    edgex = int(math.floor(window / 2.0))
-    edgey = int(math.floor(window / 2.0))
-
-    for i in range(dx):
-        for j in range(dy):
-            window_array = array[max(i - edgex, 0):min(i + edgex + 1, dx),
-                                 max(j - edgey, 0):min(j + edgey + 1, dy)]
-            new_array[i, j] = window_array.mean()
-    return new_array
-
-
-def rolling_ball_background(array, radius, light_background=True,
-                            smoothing=True):
+def rolling_ball_background(array, radius,
+                            smoothing=False, jit=True):
     """
     Calculates and subtracts background from array.
 
     Arguments:
-    array - uint8 numpy array representing image
+    array - numpy array representing image
     radius - radius of the rolling ball creating the background
-    light_background - Does image have light background
     smoothing - Whether the image should be smoothed before creating the
                 background.
     """
-    invert = False
-    if light_background:
-        invert = True
-
     ball = RollingBall(radius)
-    float_array = array
-    float_array = rolling_ball_float_background(float_array, radius, invert,
-                                                smoothing, ball)
-    background_pixels = float_array.flatten()
-
-    if invert:
-        offset = 255.5
-    else:
-        offset = 0.5
-    pixels = numpy.int8(array.flatten())
-
-    for p in range(len(pixels)):
-        value = (pixels[p] & 0xff) - (background_pixels[p] + 255) + offset
-        if value < 0:
-            value = 0
-        if value > 255:
-            value = 255
-
-        pixels[p] = numpy.int8(value)
-
-    return numpy.reshape(pixels, array.shape)
+    float_array = rolling_ball_float_background(array, radius,
+                                                smoothing, ball, jit)
+    offset = 0.5
+    final = array - float_array + offset
+    #Is this correct?
+    final[final < 0 ] = 0
+    final[final > 0xffff] = 0xffff
+    return final.astype('uint16')
 
 
-def rolling_ball_float_background(float_array, radius, invert, smoothing,
-                                  ball):
+def rolling_ball_float_background(float_array, radius, smoothing,
+                                  ball, jit):
     """
     Create background for a float image by rolling a ball over the image
     """
-    pixels = float_array.flatten()
     shrink = ball.shrink_factor > 1
 
-    if invert:
-        for i in range(len(pixels)):
-            pixels[i] = -pixels[i]
-
     if smoothing:
-        smoothed_pixels = smooth(numpy.reshape(pixels, float_array.shape))
-        pixels = smoothed_pixels.flatten()
-
-    pixels = roll_ball(ball, numpy.reshape(pixels, float_array.shape))
-
-    if invert:
-        for i in range(len(pixels)):
-            pixels[i] = -pixels[i]
+        float_array = rank.mean(float_array, selem=disk(3))
+    pixels = roll_ball(ball, float_array.astype('float32'), jit)
     return numpy.reshape(pixels, float_array.shape)
 
+@jit('f4[:](f4[:],i2,f4[:,:],f4[:],f4[:])', nopython=True)
+def run_balln(z_ball, ball_width, array, pixels, cache):
+    height, width = array.shape
+    radius = ball_width / 2
 
-def roll_ball(ball, array):
+    for y in range(-radius, height + radius):
+        next_line_to_write_in_cache = (y + radius) % ball_width
+        next_line_to_read = y + radius
+        if next_line_to_read < height:
+            src = next_line_to_read * width
+            dest = next_line_to_write_in_cache * width
+            for i in range(dest, dest+width):
+                cache[i] = pixels[src]
+                src += 1
+            p = next_line_to_read * width
+            for x in range(width):
+                pixels[p] = -float(1.7*10**10)
+                p += 1
+        y_0 = y - radius
+        if y_0 < 0:
+            y_0 = 0
+        y_ball_0 = y_0 - y + radius
+        y_end = y + radius
+        if y_end >= height:
+            y_end = height - 1
+        for x in range(-radius, width + radius):
+            z = float(1.7*10**10)
+            x_0 = x - radius
+            if x_0 < 0:
+                x_0 = 0
+            x_ball_0 = x_0 - x + radius
+            x_end = x + radius
+            if x_end >= width:
+                x_end = width - 1
+            y_ball = y_ball_0
+            for yp in range(y_0, y_end + 1):
+                cache_pointer = (yp % ball_width) * width + x_0
+                bp = x_ball_0 + y_ball * ball_width
+                for xp in range(x_0, x_end + 1):
+                    z_reduced = cache[cache_pointer] - z_ball[bp]
+                    if z > z_reduced:
+                        z = z_reduced
+                    cache_pointer += 1
+                    bp += 1
+                y_ball += 1
+
+            y_ball = y_ball_0
+            for yp in range(y_0, y_end + 1):
+                p = x_0 + yp * width
+                bp = x_ball_0 + y_ball * ball_width
+                for xp in range(x_0, x_end + 1):
+                    z_min = z + z_ball[bp]
+                    if pixels[p] < z_min:
+                        pixels[p] = z_min
+                    p += 1
+                    bp += 1
+                y_ball += 1
+
+    return pixels
+
+@jitp
+def run_ballp(z_ball, ball_width, array, pixels, cache):
+    height, width = array.shape
+    radius = ball_width / 2
+
+    for y in range(-radius, height + radius):
+        next_line_to_write_in_cache = (y + radius) % ball_width
+        next_line_to_read = y + radius
+        if next_line_to_read < height:
+            src = next_line_to_read * width
+            dest = next_line_to_write_in_cache * width
+            for i in range(dest, dest+width):
+                cache[i] = pixels[src]
+                src += 1
+            p = next_line_to_read * width
+            for x in range(width):
+                pixels[p] = -float(1.7*10**10)
+                p += 1
+        y_0 = y - radius
+        if y_0 < 0:
+            y_0 = 0
+        y_ball_0 = y_0 - y + radius
+        y_end = y + radius
+        if y_end >= height:
+            y_end = height - 1
+        for x in range(-radius, width + radius):
+            z = float(1.7*10**10)
+            x_0 = x - radius
+            if x_0 < 0:
+                x_0 = 0
+            x_ball_0 = x_0 - x + radius
+            x_end = x + radius
+            if x_end >= width:
+                x_end = width - 1
+            y_ball = y_ball_0
+            for yp in range(y_0, y_end + 1):
+                cache_pointer = (yp % ball_width) * width + x_0
+                bp = x_ball_0 + y_ball * ball_width
+                for xp in range(x_0, x_end + 1):
+                    z_reduced = cache[cache_pointer] - z_ball[bp]
+                    if z > z_reduced:
+                        z = z_reduced
+                    cache_pointer += 1
+                    bp += 1
+                y_ball += 1
+
+            y_ball = y_ball_0
+            for yp in range(y_0, y_end + 1):
+                p = x_0 + yp * width
+                bp = x_ball_0 + y_ball * ball_width
+                for xp in range(x_0, x_end + 1):
+                    z_min = z + z_ball[bp]
+                    if pixels[p] < z_min:
+                        pixels[p] = z_min
+                    p += 1
+                    bp += 1
+                y_ball += 1
+
+    return pixels
+
+
+def roll_ball(ball, array, jit=""):
     """
     Rolls a filtering object over an image in order to find the
     image's smooth continuous background.  For the purpose of explaining this
@@ -118,8 +195,15 @@ def roll_ball(ball, array):
     z_ball = ball.data
     ball_width = ball.width
     radius = ball_width / 2
-    cache = numpy.zeros(width * ball_width)
-
+    cache = numpy.zeros(width * ball_width,).astype('float32')
+    if jit.lower() in 'parakeet':
+        pixels = run_ballp(z_ball, ball_width, array, pixels, cache)
+        return numpy.reshape(pixels, array.shape)
+    elif jit.lower() in 'numba':
+        pixels = run_balln(z_ball, ball_width, array, pixels, cache)
+        return numpy.reshape(pixels, array.shape)
+    else:
+        print("No JIT")
     for y in range(-radius, height + radius):
         next_line_to_write_in_cache = (y + radius) % ball_width
         next_line_to_read = y + radius
@@ -213,3 +297,4 @@ class RollingBall(object):
                 if temp > 0:
                     self.data[p] = float(math.sqrt(temp))
                 p += 1
+        self.data = numpy.array(self.data, dtype='float32')
